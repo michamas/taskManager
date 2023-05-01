87e6e0d20f9b5b973f70c41467beffc1c818139d
Michał Masiulanis <m.masiulanis@gmail.com>
2023-05-01 22:34:10 +0200

    initial state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+const App = () => {
+  // POMOCNICZY STATE
+  const state = {
+    tasks: [
+      { id: 0, text: 'Learn HTML and CSS', completed: true },
+      { id: 1, text: 'Get good at JavaScript', completed: true },
+      { id: 2, text: 'Master React', completed: false },
+      { id: 3, text: 'Discover Redux', completed: false },
+      { id: 4, text: 'Build amazing apps', completed: false },
+    ],
+    filter: {
+      status: 'all',
+    },
+  };
+  return <div>Something</div>;
 };
+
+export default App;
